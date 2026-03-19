5ba04b0f2f9fa39190e11aeaa54061df5f5856bb
Miguel <mgroyes22@gmail.com>
2026-03-19 12:19:16 -0500

    navabarComponentInicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [NavbarComponent], // 👈 IMPORTANTE
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'estudio-digital';
 }
---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+    isOpen = false;
+
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+  }
+}
+
