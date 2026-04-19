f6a17780dac0bfec6df1003cf661a0adce360431
Miguel <mgroyes22@gmail.com>
2026-04-19 11:17:24 -0500

    AjustesProcesos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
 import { HeroComponent } from "./shared/hero/hero.component";
 import { NosotrosComponent } from "./shared/nosotros/nosotros.component";
 import { ServiciosComponent } from "./shared/servicios/servicios.component";
+import { ProcesoComponent } from "./shared/proceso/proceso.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, HeroComponent, NosotrosComponent, ServiciosComponent], // 👈 IMPORTANTE
+  imports: [NavbarComponent, HeroComponent, NosotrosComponent, ServiciosComponent, ProcesoComponent], // 👈 IMPORTANTE
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
